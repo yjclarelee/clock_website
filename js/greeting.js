515d974ff15c4216df09d893a8bdc120ebc44a41
yjclarelee <yjclarelee@postech.ac.kr>
2020-11-28 09:00:22 +0900

    Display user when local username exists

--- js/greeting.js
+++ js/greeting.js
@@ -1,7 +1,39 @@
 const textbox = document.querySelector(".js-greeting");
+const greeting = document.querySelector(".input");
+
+const USER = "user";
+
+function displayUser(){
+    const user = localStorage.getItem(USER);
+    textbox.remove();
+    greeting.innerHTML = `Hi, ${user}`;
+}
+
+function setUser(){
+    console.log("here");
+    console.log(textbox.value);
+    localStorage.setItem(USER, );
+}
+
+function getUser(){
+    console.log("getUser");
+    textbox.addEventListener("submit", setUser);
+}
+
+function setText(){
+    const user = localStorage.getItem(USER);
+    if(user){
+        displayUser();
+    }
+    else{
+        getUser();
+    }
+}
 
 
 function init(){
-    
+    // const input = textbox.innerHTML;
+    //localStorage.setItem("user", "Clare");
+    setText();
 }
 init();
